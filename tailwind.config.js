d14b4cec36aeffee60903a74d5a1dbf5d407050c
unknown <moraeslam1@gmail.com>
2023-09-05 12:14:31 +0300

    First 3 projects

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      Orange: "hsl(25, 97%, 53%)",
+      lessOrange: "hsla(25, 97%, 60%, 1)",
+      White: "hsl(0, 0%, 100%)",
+      LightGrey: "hsl(217, 12%, 63%)",
+      MediumGrey: "hsl(216, 12%, 54%)",
+      DarkBlue: "hsl(213, 19%, 18%)",
+      starDarkBlue: "hsla(213, 19%, 22%, 1)",
+      VeryDarkBlue: "hsl(216, 12%, 8%)",
+    },
+    fontFamily: {
+      overpass: "'Overpass', sans-serif",
+      hankenGrotesk: "'Hanken Grotesk', sans-serif",
+      fraunces: "'Fraunces', serif",
+      montserrat: "'Montserrat', sans-serif",
+      outfit: "'Outfit', sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
